66dfcd066e5e84214aca2e4c2c4669f2099c861e
Richard Smith-Unna <richardsmith404@gmail.com>
2015-04-22 22:01:13 +0100

    Fix typo in ddloglevel

--- Ice/AppDelegate.m
+++ Ice/AppDelegate.m
@@ -38,7 +38,7 @@
 #ifdef DEBUG
 static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
 #else
-static const DDLogLevel ddLogLevel = DDLogLevelWarn;
+static const DDLogLevel ddLogLevel = DDLogLevelWarning;
 #endif
 
 #ifdef SPARKLE
